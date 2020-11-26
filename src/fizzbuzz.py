6af7588d72dada7572d21b4b3d8ec890bd1293c7
ange47rm <ange47rm@hotmail.com>
2020-11-26 12:00:55 +0000

    5 tests passed.

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,13 +1,14 @@
 def fizzbuzz(number):
     
-    if number == 3:
+    if number == 15:
+        return "FizzBuzz"
+    
+    
+    elif number % 3 == 0:
         return "Fizz"
     
-    elif number == 5:
+    elif number % 5:
         return "Buzz"
- 
-    elif number == 15:
-        return "FizzBuzz"
 
     else:
         return str(number)
--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -15,3 +15,9 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_fizzbuzz__4_returns_4_as_str(self):
         self.assertEqual ("4", fizzbuzz(4))
+
+    def test_fizzbuzz__6_returns_fizz (self):
+        self.assertEqual ("Fizz", fizzbuzz(6))
+
+    def test_fizzbuzz__10_returns_buzz(self):
+        self.assertEqual ("Buzz", fizzbuzz(10))
